c9607b74e6772a2be4326521606712bbf7cb9599
Zippeldt <v.zippeldt@itransition.com>
2022-12-12 00:12:06 +0300

    cli: add resolved cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
 const parseArgs = () => {
-    // Write your code here 
+    console.log(
+        process.argv.slice(2).map((arg, index) =>
+            index % 2 === 0 ? `${arg.replace('--', '')} is` : `${arg},`
+        ).join(' ')
+    )
 };
 
 parseArgs();
