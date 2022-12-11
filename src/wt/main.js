1c66a6390f6f8cf9099005ccfaf80260e9452093
Zippeldt <v.zippeldt@itransition.com>
2022-12-12 00:35:26 +0300

    wt: added resolved wt task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,20 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import { Worker } from 'node:worker_threads';
+import { cpus } from 'os';
+
+const DEFAULT_START_NUMBER = 10;
+
+const getNthFibonacciNumber = (data, workerFilePath = './src/wt/worker.js') => new Promise((resolve, reject) => {
+    const newWorker = new Worker(workerFilePath, { workerData: data });
+
+    newWorker.on('message', (data) => resolve({status: 'resolved', data}))
+    newWorker.on('error',() => reject({status: 'error', data: null}))
+});
+
+const performCalculations = async () =>
+    console.log(
+        await Promise.all(
+            cpus().map((item,index) => getNthFibonacciNumber(index + DEFAULT_START_NUMBER))
+        )
+    );
 
 await performCalculations();
