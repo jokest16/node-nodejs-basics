5da3b3be021303b954df59418535da4fe0920dc7
Zippeldt <v.zippeldt@itransition.com>
2022-12-12 00:30:17 +0300

    hash: added resolved has task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,12 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import { createHash } from 'node:crypto';
+import { readFile } from 'fs/promises';
+
+const calculateHash = async (hashFilePath = 'src/hash/files/fileToCalculateHashFor.txt') =>
+    await readFile(hashFilePath)
+        .then((currentDirContent) =>
+            console.log(
+                createHash('sha256').update(currentDirContent.toString()).digest('hex')
+            )
+        );
 
 await calculateHash();
