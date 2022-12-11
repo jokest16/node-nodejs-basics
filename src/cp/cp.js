aba429b2f16f9bc3d60ae630a7c5dfddce250ef6
Zippeldt <v.zippeldt@itransition.com>
2022-12-12 00:25:54 +0300

    cp added resolved cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,11 @@
+import { stdin, stdout } from 'node:process';
+import { fork } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const { stdin: childStdin, stdout: childStdout  } = fork('./src/cp/files/script.js', args, { silent: true });
+
+    stdin.pipe(childStdin);
+    childStdout.pipe(stdout);
 };
 
 // Put your arguments in function call to test this functionality
