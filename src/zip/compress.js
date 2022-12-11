097ee78bc9fb2a1da7ce5f2dad676568711611d5
Zippeldt <v.zippeldt@itransition.com>
2022-12-12 00:41:39 +0300

    zip: added resolved zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
-const compress = async () => {
-    // Write your code here 
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+
+const compress = async (
+    filePath = 'src/zip/files/fileToCompress.txt',
+    compressedFileDir = 'src/zip/files',
+    gzipFileName = 'archive'
+) => {
+    const newGzip = createGzip();
+    const readFileToCompressStream = createReadStream(filePath);
+    const compressedFileStream = createWriteStream(`${compressedFileDir}/${gzipFileName}.gz`);
+
+    readFileToCompressStream.pipe(newGzip).pipe(compressedFileStream);
 };
 
 await compress();
